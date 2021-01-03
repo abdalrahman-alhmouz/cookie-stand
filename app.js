16496f443f32c60cfb3cc3325f4f4389ba090454
abdalrahman-alhmouz <abood.alhmouz121@gmail.com>
2021-01-03 22:42:24 +0200

    init comit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+alert("g")
 var resturan ={
     location :'Seattle',
     minHourlyCustomers : 23 ,
     maxHourlyCustomers : 65 , 
     averageCookiesPer : 6.3, 
-
-
+    array :['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'] ,
 }
 function getRandomInt(min, max) {
     min = resturan.minHourlyCustomers
@@ -14,13 +14,17 @@ function getRandomInt(min, max) {
   getRandomInt(resturan.minHourlyCustomers, resturan.maxHourlyCustomers) ;
 console.log(getRandomInt());
 
+var parentelement = document.getElementById("cookies list");
+var artile = document.createElement("article")
+parentelement.appendChild(artile) ;
+var list =document.createElement("ul")
+artile.appendChild(list);
+
 var resturan1 ={
     location :'tokyo' ,
     minHourlyCustomers : 3 ,
     maxHourlyCustomers : 24 ,
     averageCookiesPer : 1.2 ,
-
-
 }
 function getRandomIn(min, max) {
     min = resturan1.minHourlyCustomers
@@ -30,11 +34,16 @@ function getRandomIn(min, max) {
   getRandomIn(resturan1.minHourlyCustomers, resturan1.maxHourlyCustomers) ;
 console.log(getRandomIn());
 
+for (var index=0 ; index < resturan1.array.length ; index++){
+  var listone= document.createElement("li") ;
+  listone.textContent =resturan1.array[index]+" "+ getRandomIn() /resturan1.averageCookiesPer +" cookies" ;
+  list.appendChild(listone) ;
+
 var resturan2 ={
-    location :'Dubai' ,
-    minHourlyCustomers : 11 ,
-    maxHourlyCustomers : 38 ,
-    averageCookiesPer : 6.3 ,
+      location :'Dubai' ,
+      minHourlyCustomers : 11 ,
+      maxHourlyCustomers : 38 ,
+      averageCookiesPer : 6.3 ,
 
 
 }
@@ -50,7 +59,7 @@ var resturan3 ={
     minHourlyCustomers : 20 ,
     maxHourlyCustomers : 83 ,
     averageCookiesPer : 6.3 ,
-
+   
 
 }
 function getRandom(min, max) {
