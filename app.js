a34db45cd809edf053c458b933d2868a896ca9c1
abdalrahman-alhmouz <abood.alhmouz121@gmail.com>
2021-01-03 15:47:51 +0200

    init comit

---
+++ app.js
@@ -0,0 +1,79 @@
+var resturan ={
+    location :'Seattle',
+    minHourlyCustomers : 23 ,
+    maxHourlyCustomers : 65 , 
+    averageCookiesPer : 6.3, 
+
+
+}
+function getRandomInt(min, max) {
+    min = resturan.minHourlyCustomers
+    max = resturan.maxHourlyCustomers
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
+  getRandomInt(resturan.minHourlyCustomers, resturan.maxHourlyCustomers) ;
+console.log(getRandomInt());
+
+var resturan1 ={
+    location :'tokyo' ,
+    minHourlyCustomers : 3 ,
+    maxHourlyCustomers : 24 ,
+    averageCookiesPer : 1.2 ,
+
+
+}
+function getRandomIn(min, max) {
+    min = resturan1.minHourlyCustomers
+    max = resturan1.maxHourlyCustomers
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
+  getRandomIn(resturan1.minHourlyCustomers, resturan1.maxHourlyCustomers) ;
+console.log(getRandomIn());
+
+var resturan2 ={
+    location :'Dubai' ,
+    minHourlyCustomers : 11 ,
+    maxHourlyCustomers : 38 ,
+    averageCookiesPer : 6.3 ,
+
+
+}
+function getRandomI(min, max) {
+    min = resturan2.minHourlyCustomers
+    max = resturan2.maxHourlyCustomers
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
+  getRandomI(resturan2.minHourlyCustomers, resturan2.maxHourlyCustomers) ;
+console.log(getRandomI());
+var resturan3 ={
+    location :'Paris' ,
+    minHourlyCustomers : 20 ,
+    maxHourlyCustomers : 83 ,
+    averageCookiesPer : 6.3 ,
+
+
+}
+function getRandom(min, max) {
+    min = resturan3.minHourlyCustomers
+    max = resturan3.maxHourlyCustomers
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
+  getRandom(resturan3.minHourlyCustomers, resturan3.maxHourlyCustomers) ;
+console.log(getRandom());
+
+var resturan4 ={
+    location :'lima' ,
+    minHourlyCustomers : 2 ,
+    maxHourlyCustomers : 16 ,
+    averageCookiesPer : 6.3 ,
+
+
+}
+function getRando(min, max) {
+    min = resturan4.minHourlyCustomers
+    max = resturan4.maxHourlyCustomers
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
+  getRando(resturan4.minHourlyCustomers, resturan4.maxHourlyCustomers) ;
+console.log(getRando());
+
